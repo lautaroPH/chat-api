3f8902e88dd5b21656783535015c067d4ed1cb02
Lautaro Perez Herrera <lautaroph@gmail.com>
2023-06-10 16:21:55 -0300

    add cors

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { chatCompletion } from './chat-completion.js';
 import { PORT } from './config.js';
-
+import cors from 'cors';
 const app = express();
 
+app.use(cors());
 app.use(express.json()); // Agrega esta línea
 app.use('/', (req, res, next) => {
   res.status(200).json({ message: 'Hello world' });
