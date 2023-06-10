5a5757e8a8e3967fa95fc2a617e8f1339f5e4e1d
Lautaro Perez Herrera <lautaroph@gmail.com>
2023-06-10 16:05:53 -0300

    fox

--- index.js
+++ index.js
@@ -1,26 +1,13 @@
 import express from 'express';
 import { chatCompletion } from './chat-completion.js';
-import cors from 'cors';
 
 const app = express();
 const port = 4000;
 
-const whitelist = ['http://localhost:3000', 'https://www.chenkster.xyz'];
-
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  optionsSuccessStatus: 200,
-  methods: 'POST',
-};
-
-app.use(cors(corsOptions));
 app.use(express.json()); // Agrega esta línea
+app.use('/', (req, res, next) => {
+  res.status.json({ message: 'Hello world' });
+});
 app.post('/', chatCompletion);
 
 app.listen(port, () => {
